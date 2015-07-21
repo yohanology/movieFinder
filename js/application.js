a94db1109b882bf7a08b4dc4d85f9e52070df1c4
Yohan <yohanology@gmail.com>
2015-07-21 13:50:11 +0800

    loading animation, ajax function

---
+++ js/application.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+$(document).on('keydown','input[name="movie-title"]',function(e){
+	if (e.keyCode === 13) {
+		console.log('keyDown')
+
+		$(document).ajaxStart(function() {
+		  $("#loading-gif").show();
+		});
+
+		$(document).ajaxStop(function() {
+		  $("#loading-gif").hide();
+		});
+
+		var query = $('input[name="movie-title"]').val();
+		
+		$.ajax({
+			type: "GET",
+			url: "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&r=json",
+			dataType: "JSON",
+			success: function(response) {
+				$('.movie-poster').attr('src', response.Poster);
+				$('.movie-title').text('Title ' + response.Title);
+				$('.movie-year').text('Year ' + response.Year);
+				$('.movie-rated').text('Rated ' + response.Rated);
+				$('.movie-released').text('Released ' + response.Released);
+				$('.movie-runtime').text('Runtime ' + response.Runtime);
+				$('.movie-genre').text('Genre ' + response.Genre);
+				$('.movie-director').text('Director ' + response.Director);
+				$('.movie-writer').text('Writer ' + response.Writer);
+				$('.movie-actors').text('Actors ' + response.Actors);
+				$('.movie-language').text('Language ' + response.Language);
+				$('.movie-country').text('Country ' + response.Country);
+				$('.movie-awards').text('Awards ' + response.Awards);
+
+			}
+		})
+	}
+})
+
+
+
+
+
+})
